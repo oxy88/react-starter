6947cf329f6dadff78648d09d84de8bdb461a9fc
oxy88 <oxyoxy88@gmail.com>
2017-10-27 13:38:28 +0900

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 // Change All
 
-export const SIMPLE_API = 'https://api.graph.cool/simple/v1/cj99e5mqs7dgw0115okwehp2s'
-export const SUBSCRIPTIONS_API = 'wss://subscriptions.graph.cool/v1/cj99e5mqs7dgw0115okwehp2s'
-export const TOKEN = 'GC_TOKEN' 
+export const SIMPLE_API = ''
+export const SUBSCRIPTIONS_API = ''
+export const TOKEN = '' 
